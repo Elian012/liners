fbbb183022da0af152ad3782a0d91ef935abb980
Your Name <you@example.com>
2024-03-11 11:06:32 +0200

    Multiply table function

---
+++ main_mul_table.py
@@ -0,0 +1,15 @@
+"""
+ Author: Elian
+ Purpose: Runs the function to check if it works.
+ Date: 11.03.2024
+"""
+from liners.one_line_func_mul_table import mul_table
+
+
+def main():
+    """Entry point"""
+    print(mul_table(3))
+
+
+if __name__ == '__main__':
+    main()
